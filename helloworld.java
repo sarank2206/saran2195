5f73203429ed0fd2cf971076603f6c52e7210fd7
sarank2206 <52885241+sarank2206@users.noreply.github.com>
2019-08-02 17:31:21 +0530

    1st commit

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class JenkinsExample {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello Agni");
+System.out.println("Hello ni");
+ }
+
+}
